017e3c4ba5a8a15d66de19b87ecc01eb0d2d251c
Thanh Tong <truongthanhh88@gmail.com>
2025-02-22 01:52:39 +0700

    fix

--- src/components/HeaderComponents/LanguageSelector.tsx
+++ src/components/HeaderComponents/LanguageSelector.tsx
@@ -3,42 +3,6 @@ import { useState } from "react"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
 import { Button } from "../ui/button"
 
-// SVG flags for languages
-const VietnamFlag = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 600" className="w-5 h-5">
-        <rect width="900" height="600" fill="#da251d" />
-        <polygon points="450,150 585,450 315,450" fill="#ff0" />
-    </svg>
-)
-
-const USFlag = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 600" className="w-5 h-5">
-        <rect width="900" height="600" fill="#b22234" />
-        <path
-            d="M0,69.8h900v60.4H0zm0,120.8h900v60.4H0zm0,120.8h900v60.4H0zm0,120.8h900v60.4H0zm0,120.8h900v60.4H0zm0,120.8h900v60.4H0"
-            fill="#fff"
-        />
-        <rect width="360" height="320" fill="#3c3b6e" />
-        <g fill="#fff">
-            <g id="s18">
-                <g id="s9">
-                    <g id="s5">
-                        <g id="s4">
-                            <path id="s" d="M24,24l2,6h6l-5,4l2,6l-5-4l-5,4l2-6l-5-4h6z" />
-                            <use href="#s" y="36" />
-                            <use href="#s" y="72" />
-                        </g>
-                        <use href="#s4" y="108" />
-                    </g>
-                    <use href="#s5" x="72" />
-                </g>
-                <use href="#s9" x="144" />
-            </g>
-            <use href="#s18" y="144" />
-        </g>
-    </svg>
-)
-
 interface Language {
     code: string
     name: string
