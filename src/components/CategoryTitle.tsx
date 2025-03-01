354e6e35de9bbf00d37a8f57be4d28d608d062db
MeoKool <truongsyquang1202@gmail.com>
2025-03-01 17:49:18 +0700

    refactor: update CategoryTitle and OrderItem components for improved styling and code consistency

--- src/components/CategoryTitle.tsx
+++ src/components/CategoryTitle.tsx
@@ -1,17 +1,17 @@
 import type React from 'react'
 
 interface CategoryTitleProps {
-  categories: { id: number; name: string }[]
+  categories: { id: number; name: string; description?: string }[]
 }
 
 const CategoryTitle: React.FC<CategoryTitleProps> = ({ categories }) => {
   return (
-    <div className='flex space-x-4 overflow-x-auto py-2 bg-orange-400 shadow-md rounded-lg'>
+    <div className='flex space-x-4 overflow-x-auto py-2 bg-white shadow-md rounded-lg'>
       {categories.map((item) => (
         <div
           key={item.id}
           id={`category-${item.id}`}
-          className='flex-shrink-0 px-4 py-2  text-white cursor-pointer transition duration-300 ease-in-out transform hover:scale-105'
+          className='flex-shrink-0 px-4 py-2 text-gray-700 hover:bg-gray-300 cursor-pointer transition duration-300 ease-in-out transform hover:scale-105'
         >
           {item.name}
         </div>
